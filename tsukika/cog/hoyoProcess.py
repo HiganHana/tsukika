6050beffd48fcf0ae5c1f91976b6fb76c3632196
celtica <celticaxp@gmail.com>
2022-10-03 21:11:15 -0400

    feat(dcog/honkai): hoyolab

---
+++ tsukika/cog/hoyoProcess.py
@@ -0,0 +1,56 @@
+import typing
+import genshin
+from tsukika.bot import mf
+from fuzzywuzzy import process
+
+class hoyoProcessor:
+    def __init__(self) -> None:
+        self.hoyo_client = genshin.Client({
+            "ltuid": mf.config.hoyo_ltuid,
+            "ltoken": mf.config.hoyo_ltoken,
+        })
+        
+        self.close_matches = {}
+
+    async def get_honkai_all_battlesuits(self, uid : int) -> typing.List[genshin.models.FullBattlesuit]:
+        try:
+            return await self.hoyo_client.get_honkai_battlesuits(uid), True
+        except:
+            return None, False
+    
+    async def get_honkai_character(self, uid : int, character : typing.Union[int, str]) -> typing.Optional[genshin.models.FullBattlesuit]:
+        try:
+            battlesuits = await self.hoyo_client.get_honkai_battlesuits(uid)
+        except:
+            return None, False
+        
+        for b in battlesuits:
+            if isinstance(character, int) and b.id == character:
+                return b, True
+            elif isinstance(character, str) and b.name.lower() == character.lower().strip():    
+                return b, True
+            elif (
+                isinstance(character, str) 
+                and character.lower().strip() in self.close_matches
+                and b.name.lower() == self.close_matches[character.lower().strip()].lower()
+            ):
+                return b, True
+    
+        # do fuzzy matching
+        if not isinstance(character, int):
+            return None, True
+        
+        characters_owned = [b.name for b in battlesuits]
+        close_match = process.extractOne(character, characters_owned)
+        self.close_matches[character] = close_match[0].name
+
+        return close_match, True
+    
+HOYO_PROCESSOR = hoyoProcessor()
+
+            
+
+        
+        
+        
+    
