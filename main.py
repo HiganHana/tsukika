12fa0ecf854ce39432f112078eba5214e8d9bad5
celtica <celticaxp@gmail.com>
2022-10-03 21:52:14 -0400

    env: main run

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 from tsukika.bot import mf, base
 
+
 if __name__ == "__main__":
     mf.setup_bot()
     mf.setup_flask()
     mf.setup_flask_sqlalchemy(_declarative_base=base)
         
     mf.run()
-    
